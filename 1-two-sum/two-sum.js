3d6b2c807f8bd2eb6beae411421a631abb3b6cf5
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-08 19:45:08 +0530

    Time: 71 ms (50.96%) | Memory: 52.8 MB (5.10%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,22 +4,22 @@
  * @return {number[]}
  */
 //  Solution 1
-// var twoSum = function(numbers, targetSum) {
-//     let numIndicesMap = {};
-//     for (let [currentIndex, currentNum] of numbers.entries()) {
-//         if (numIndicesMap[currentNum] !== undefined) {
-//             return [numIndicesMap[currentNum], currentIndex];
-//         }
-//         numIndicesMap[targetSum - currentNum] = currentIndex;
-//     }
-// };
-
-//  Solution 2
-var twoSum = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            // logic
-            if (nums[i] + nums[j] === target) return [i, j];
+var twoSum = function(numbers, targetSum) {
+    let numIndicesMap = {};
+    for (let [currentIndex, currentNum] of numbers.entries()) {
+        if (numIndicesMap[currentNum] !== undefined) {
+            return [numIndicesMap[currentNum], currentIndex];
         }
+        numIndicesMap[targetSum - currentNum] = currentIndex;
     }
 };
+
+//  Solution 2
+// var twoSum = function (nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             // logic
+//             if (nums[i] + nums[j] === target) return [i, j];
+//         }
+//     }
+// };
