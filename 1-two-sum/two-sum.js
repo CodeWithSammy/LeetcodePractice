adc89e19ab24c6d419580f178e990aa337ef6d05
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-08 19:44:56 +0530

    Time: 101 ms (34.13%) | Memory: 49.4 MB (65.63%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+//  Solution 1
+// var twoSum = function(numbers, targetSum) {
+//     let numIndicesMap = {};
+//     for (let [currentIndex, currentNum] of numbers.entries()) {
+//         if (numIndicesMap[currentNum] !== undefined) {
+//             return [numIndicesMap[currentNum], currentIndex];
+//         }
+//         numIndicesMap[targetSum - currentNum] = currentIndex;
+//     }
+// };
+
+//  Solution 2
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            // logic
+            if (nums[i] + nums[j] === target) return [i, j];
+        }
+    }
+};
