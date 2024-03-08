454820533ad1d00e9675a14029c7f3771e2118a5
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-08 19:28:07 +0530

    Time: 73 ms (54.56%) | Memory: 50.6 MB (71.40%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -12,16 +12,27 @@
 
 //  Solution 2
 var isAnagram = function(s, t) {
-    if(s.length !== t.length) return false;
-    let obj1 ={};
-    let obj2 ={};
-    for( let i =0; i< s.length; i++){
-        obj1[s[i]] = (obj1[s[i]] || 0)+1;
-        obj2[t[i]] = (obj2[t[i]] || 0)+1;
+    // Check if the lengths of the two strings are different, they cannot be anagrams
+    if (s.length !== t.length) return false;
+    
+    // Create two objects to store the frequency of characters in each string
+    let obj1 = {};
+    let obj2 = {};
+    
+    // Count the frequency of characters in the first string, s
+    for (let i = 0; i < s.length; i++) {
+        // Increment the count of character s[i] in obj1
+        obj1[s[i]] = (obj1[s[i]] || 0) + 1; 
+        // Count the frequency of characters in the second string, t
+        obj2[t[i]] = (obj2[t[i]] || 0) + 1;
     }
-    for( const key in obj1){
-        if(obj1[key] !== obj2[key]) return false
+    
+    // Compare the frequencies of characters in obj1 and obj2
+    for (const key in obj1) {
+        // If the frequencies of corresponding characters in s and t are different, return false
+        if (obj1[key] !== obj2[key]) return false;
     }
-    return true
-
+    
+    // If all characters have the same frequencies in both strings, return true (they are anagrams)
+    return true;
 };
