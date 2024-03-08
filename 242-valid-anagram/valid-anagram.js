15b395ecf503d5a5bc7b08a96cc90e5955ded357
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-08 19:25:11 +0530

    Time: 64 ms (81.86%) | Memory: 50.7 MB (69.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,8 +3,25 @@
  * @param {string} t
  * @return {boolean}
  */
+//  Solution 1
+// var isAnagram = function(s, t) {
+//     s = s.split("").sort().join("");
+//     t = t.split("").sort().join("");
+//     return s ===t;
+// };
+
+//  Solution 2
 var isAnagram = function(s, t) {
-    s = s.split("").sort().join("");
-    t = t.split("").sort().join("");
-    return s ===t;
+    if(s.length !== t.length) return false;
+    let obj1 ={};
+    let obj2 ={};
+    for( let i =0; i< s.length; i++){
+        obj1[s[i]] = (obj1[s[i]] || 0)+1;
+        obj2[t[i]] = (obj2[t[i]] || 0)+1;
+    }
+    for( const key in obj1){
+        if(obj1[key] !== obj2[key]) return false
+    }
+    return true
+
 };
