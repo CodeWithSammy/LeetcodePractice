ded332d5337a63a372720b6b72e44d00ae6b8c88
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-08 19:04:41 +0530

    Time: 50 ms (80.05%) | Memory: 48.4 MB (66.95%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -2,14 +2,17 @@
  * @param {number} n
  * @return {number}
  */
-// var fib = function(n) {
-//     const arr = [0,1];
-//     for( let i =2; i<=n; i++){
-//         arr.push(arr[i-1] +arr[i-2])
-//     }
-//     return arr[n];
-// };
+
+//  solution 1
 var fib = function(n) {
-    if( n<=1) return n;
-    return fib(n-1)+ fib(n-2);
+    const arr = [0,1];
+    for( let i =2; i<=n; i++){
+        arr.push(arr[i-1] +arr[i-2])
+    }
+    return arr[n];
 };
+//  solution 2
+// var fib = function(n) {
+//     if( n<=1) return n;
+//     return fib(n-1)+ fib(n-2);
+// };
