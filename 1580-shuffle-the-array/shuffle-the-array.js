6898ee44102c9c32230c2600aad3d9fa9cb9b5e9
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-10 21:05:10 +0530

    Time: 66 ms (28.01%) | Memory: 50.6 MB (79.13%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.js
+++ 1580-shuffle-the-array/shuffle-the-array.js
@@ -4,9 +4,15 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    let res =[];
-    for( let i = 0; i< n; i++){
-        res.push(nums[i], nums[i+n])
+    // Initialize an empty array to store the shuffled result
+    let res = [];
+    // Iterate over the first 'n' elements of the array
+    for (let i = 0; i < n; i++) {
+        // Push the i-th element from the first half of the array
+        res.push(nums[i]);
+        // Push the i-th element from the second half of the array
+        res.push(nums[i + n]);
     }
+    // Return the shuffled array
     return res;
 };
