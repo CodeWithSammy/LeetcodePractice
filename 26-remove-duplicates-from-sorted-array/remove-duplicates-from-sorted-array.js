9e3d93c3c933c3fe70442f2aa4d84da1f930c743
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-10 13:19:20 +0530

    Time: 68 ms (43.14%) | Memory: 52.5 MB (33.39%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,16 +3,23 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+    // Check if the array is empty
     if (nums.length === 0) {
-        return 0;
+        return 0; // If empty, return 0 as there are no elements
     }
 
-    let j = 0;
+    let j = 0; // Initialize a pointer j to keep track of unique elements
+    // Iterate through the array starting from the second element (index 1)
     for (let i = 1; i < nums.length; i++) {
+        // Check if the current element is different from the element at index j
         if (nums[j] !== nums[i]) {
+            // If different, increment j and update the element at index j+1 with the current element
             nums[++j] = nums[i];
         }
+        // If the current element is the same as the element at index j, skip it
     }
 
+    // Return the length of the array with duplicates removed (j + 1)
+    // Adding 1 to j because j is a zero-based index
     return j + 1;
 };
