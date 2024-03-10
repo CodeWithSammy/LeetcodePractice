f8e0b444c886860e6daac7f166d59d5325e2e7b3
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-10 12:53:29 +0530

    Time: 70 ms (92.52%) | Memory: 57.3 MB (48.81%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -3,10 +3,17 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-    while (nums.length <= k) {
+var rotate = function (nums, k) {
+    let size = nums.length;
+    let temp;
+while (nums.length <= k) {
         k = k - nums.length;
     }
-    const temp = nums.splice(0, nums.length-k)
-    nums.push(...temp)
+    
+    if (size > k) {
+        k = k % size;
+    }
+    const rotated = nums.splice(size - k, size);
+    nums.unshift(...rotated);
+    return nums;
 };
