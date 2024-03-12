25745080b9e17569a6937b2d7bbc788bae883893
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-12 09:39:59 +0530

    Time: 117 ms (53.48%) | Memory: 64.1 MB (14.54%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let result = new Map();
+    for( let str of strs){
+        let count = Array(26).fill(0);
+        for(let char of str ){
+            count[char.charCodeAt(0)- 'a'.charCodeAt(0)]++;
+        }
+        let key = count.join('-');
+        if (!result.has(key)) {
+            result.set(key, []); // Initialize an empty array for the key if it doesn't exist
+        }
+        result.get(key).push(str);
+    }
+    return Array.from(result.values());
+};
