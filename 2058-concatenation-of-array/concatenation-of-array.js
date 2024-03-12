296f032007eec7bf85906a7ae394fe9fc83a505c
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-12 11:18:46 +0530

    Time: 72 ms (24.83%) | Memory: 52.5 MB (19.52%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -6,6 +6,15 @@
 //     return [...nums ,...nums];
 // };
 
-var getConcatenation = function (nums) {
-  return nums.concat(nums);
+// var getConcatenation = function (nums) {
+//   return nums.concat(nums);
+// };
+
+var getConcatenation = function(nums) {
+    const result = [];
+    for(let i= 0;  i< nums.length; i++) {
+        result[i] = nums[i];
+        result [i + nums.length] = nums[i]
+    }
+    return result;
 };
