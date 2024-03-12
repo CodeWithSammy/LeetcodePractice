5aa23fb89465fc04cee78e11c612933901b9eefd
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-12 11:24:04 +0530

    Time: 70 ms (34.41%) | Memory: 53 MB (7.26%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -20,10 +20,16 @@
 //     return ans;
 // };
 
-var getConcatenation = function (nums) {
+var getConcatenation = function(nums) {
+    // Initialize an empty array to store the concatenated result
     let res = [];
+
+    // Loop from 0 to twice the length of the input array nums
     for (let i = 0; i < nums.length * 2; i++) {
+        // Push the element at the current index % length of nums into res
         res.push(nums[i % nums.length]);
     }
+
+    // Return the concatenated array
     return res;
 };
