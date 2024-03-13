c36429038026f688761d3573977c69d60d87705a
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-13 12:05:33 +0530

    Time: 56 ms (52.69%) | Memory: 51.6 MB (12.04%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -3,22 +3,30 @@
  * @return {string}
  */
 const reverseWords = function (s) {
+  // Split the string by spaces to get individual words
   const splitS = s.split(" ");
+  // Create a stack to store the words in reverse order
   const stack = [];
 
+  // Push each word from the split string into the stack
   for (let i of splitS) {
     stack.push(i);
   }
 
+  // Initialize an empty string to store the final reversed string
   let finalS = "";
 
+  // Pop words from the stack and append them to the final string
   while (stack.length) {
+    // Pop the top word from the stack
     const current = stack.pop();
 
+    // If the popped word is not empty, append it to the final string
     if (current) {
       finalS += " " + current;
     }
   }
 
+  // Trim any leading or trailing spaces from the final string and return it
   return finalS.trim();
 };
