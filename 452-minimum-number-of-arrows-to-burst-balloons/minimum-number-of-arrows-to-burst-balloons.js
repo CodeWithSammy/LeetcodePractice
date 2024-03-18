840289861ad249b1a26b913887e0a8367177a796
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-18 20:03:15 +0530

    Time: 240 ms (13.34%) | Memory: 71.8 MB (96.69%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
@@ -6,12 +6,13 @@ var findMinArrowShots = function(points) {
     points.sort((a, b) => a[0] - b[0]);
     let countOfArrows = points.length;
     let previous = points[0];
-
+// the loop iteration to start from 1 instead of 0 since previous is initialized with points[0].
     for( i =1; i< points.length; i++){
         let current = points[i];
         if( current[0] <= previous[1]){
             countOfArrows -= 1;
             previous = [ current[0], Math.min(current[1], previous[1])]
+            
         }
         else{
             previous = current;
@@ -19,3 +20,4 @@ var findMinArrowShots = function(points) {
     }
     return countOfArrows;
 };
+
