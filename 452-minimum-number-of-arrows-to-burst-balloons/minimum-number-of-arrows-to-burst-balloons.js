fd1935fa3a8ea13c2ee97efaedaf1d354f93c816
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-18 20:03:47 +0530

    Time: 234 ms (18.52%) | Memory: 74.4 MB (10.69%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
@@ -2,19 +2,21 @@
  * @param {number[][]} points
  * @return {number}
  */
-var findMinArrowShots = function(points) {
+var findMinArrowShots = function (points) {
     points.sort((a, b) => a[0] - b[0]);
     let countOfArrows = points.length;
     let previous = points[0];
-// the loop iteration to start from 1 instead of 0 since previous is initialized with points[0].
-    for( i =1; i< points.length; i++){
+    // the loop iteration to start from 1 instead of 0 since previous is initialized with points[0].
+    for (i = 1; i < points.length; i++) {
         let current = points[i];
-        if( current[0] <= previous[1]){
+        if (current[0] <= previous[1]) {
             countOfArrows -= 1;
-            previous = [ current[0], Math.min(current[1], previous[1])]
-            
+            previous = [current[0], Math.min(current[1], previous[1])]
+            // [1,5] [2,6] -> [2,5]
+            // [1,5] [2,4] -> [2,4]
+            // taking the minimum for getting the merged array
         }
-        else{
+        else {
             previous = current;
         }
     }
