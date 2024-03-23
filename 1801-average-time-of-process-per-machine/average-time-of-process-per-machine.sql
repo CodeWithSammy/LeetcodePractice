d5e0ff33441880d765eebda35c47002bf3dae930
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-23 13:03:13 +0530

    Time: 640 ms (11.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+ SELECT a1.machine_id,ROUND(SUM(a2.timestamp - a1.timestamp)/ COUNT(*),3) AS processing_time  
+ FROM Activity a1
+ INNER JOIN Activity a2
+ ON a1.machine_id = a2.machine_id
+ AND a1.process_id = a2.process_id
+ AND a1.timestamp < a2.timestamp
+ GROUP BY a1.machine_id
