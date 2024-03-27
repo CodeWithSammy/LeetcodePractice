1e23c5b109ff3f7ae07c77d7e6ad095ad47b9724
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-27 09:26:24 +0530

    Time: 54 ms (82.64%) | Memory: 51.4 MB (58.97%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let half = nums.length/2;
+     let map = {};
+     for(let i =0; i< nums.length; i++){
+        map[nums[i]] = (map[nums[i]] | 0 ) +1
+        if( map[nums[i]] > half){
+            return nums[i]
+        }
+     }
+};
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+1. Create half variable.
+2. Create map object.
+3. Loop through nums.
+    a. Add the key and count to the map.
+    b. Condition if the count is greater than half.
+        i. Return that key.
+*/
