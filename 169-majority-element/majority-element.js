63982ba9ea1000a58da5ed5b5d81af30149cddb8
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-27 09:27:59 +0530

    Time: 70 ms (16.48%) | Memory: 51.1 MB (64.86%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,15 +3,25 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let half = nums.length/2;
-     let map = {};
-     for(let i =0; i< nums.length; i++){
-        map[nums[i]] = (map[nums[i]] | 0 ) +1
-        if( map[nums[i]] > half){
-            return nums[i]
+    // Calculate the threshold for majority (more than half the length of the array)
+    let half = nums.length / 2;
+    
+    // Initialize an empty object to store the count of each element
+    let map = {};
+    
+    // Iterate through the array
+    for (let i = 0; i < nums.length; i++) {
+        // Increment the count for the current element in the map
+        map[nums[i]] = (map[nums[i]] || 0) + 1;
+        
+        // Check if the count of the current element exceeds the threshold for majority
+        if (map[nums[i]] > half) {
+            // If yes, return the current element as the majority element
+            return nums[i];
         }
-     }
+    }
 };
+
 
 /*
 Time Complexity: O(n)
