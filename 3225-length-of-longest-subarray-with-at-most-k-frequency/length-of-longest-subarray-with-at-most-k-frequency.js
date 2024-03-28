a79414d79a6bedda9c1f1eee40f2e7cb94e97250
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-03-28 10:27:08 +0530

    Time: 176 ms (12.39%) | Memory: 64.2 MB (84.07%) - LeetSync

---
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubarrayLength = function (nums, k) {
+    let ans = 0;
+    let start = -1;
+    let frequency = {};
+
+    for (let end = 0; end < nums.length; end++) {
+        // Initialize frequency of nums[end] as 0 if it's not already initialized
+        if (!frequency[nums[end]]) {
+            frequency[nums[end]] = 0;
+        }
+        frequency[nums[end]] += 1;
+        while (frequency[nums[end]] > k) {
+            start++;
+            frequency[nums[start]] -= 1;
+        }
+        ans = Math.max(ans, end - start)
+    }
+    return ans;
+};
