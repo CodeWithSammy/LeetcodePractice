bffab1e9630c4a57ab1f8fff836403d14f4b8700
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-04-01 01:55:12 +0530

    Time: 630 ms (50.28%) | Memory: 81.5 MB (65.31%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+    const result = [];
+    const deque = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        // O(n)
+        if (deque.length > 0 && deque[0] <= i - k) {
+            deque.shift();
+        }
+
+        while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
+        }
+
+        deque.push(i);
+
+        if (i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+
+    return result;
+};
