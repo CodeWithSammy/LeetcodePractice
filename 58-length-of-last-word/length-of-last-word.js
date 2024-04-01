e4bc42f5af2f13d025a8cea1207b7afaad07197b
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-04-01 16:47:33 +0530

    Time: 41 ms (95.54%) | Memory: 48.4 MB (91.14%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,15 +3,19 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
+    // Initialize index pointer at the end of the string
     let i = s.length - 1;
+    // Initialize the length of the last word
     let length = 0;
+    // Move the pointer to the last non-space character
     while (s[i] == " ") {
         i -= 1;
     }
-    while ( i>=0 && s[i] !== " ") {
+    // Count the length of the last word by moving the pointer
+    while (i >= 0 && s[i] !== " ") {
         length += 1;
         i -= 1;
-
     }
+    // Return the length of the last word
     return length;
 };
