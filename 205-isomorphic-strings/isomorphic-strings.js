ff1c3e60bf0135b7865262ae48f4f1077d768e33
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-04-02 18:15:33 +0530

    Time: 56 ms (80.50%) | Memory: 50.9 MB (46.33%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -4,20 +4,28 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
+    // Create two maps to store mappings between characters of strings s and t
     let mapST = new Map();
     let mapTS = new Map();
 
+    // Iterate over the characters of strings s and t using a loop
     for (let i = 0; i < s.length; i++) {
+        // Get the current characters from strings s and t
         const c1 = s[i];
         const c2 = t[i];
 
+        // Check if the current characters are already mapped in the maps
         if ((mapST.has(c1) && mapST.get(c1) !== c2) || (mapTS.has(c2) && mapTS.get(c2) !== c1)) {
+            // If the mappings are inconsistent, return false
             return false;
         }
         
+        // Update the mappings in both maps
         mapST.set(c1, c2);
         mapTS.set(c2, c1);
     }
 
+    // If all mappings are consistent, return true
     return true;
 };
+
