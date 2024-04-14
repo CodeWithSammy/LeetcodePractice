39c19130f6a62dcd7f9477d2713adcd48b07b0e5
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-04-14 23:54:41 +0530

    Time: 59 ms (55.44%) | Memory: 52.2 MB (6.07%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.js
+++ 404-sum-of-left-leaves/sum-of-left-leaves.js
@@ -11,21 +11,35 @@
  * @return {number}
  */
 var sumOfLeftLeaves = function(root) {
+    // Initialize a queue with the root node
     const que = [root];
-    let sum =0;
-    while(que.length >0){
-        const node =que.shift();
-        if(node.left){
-            if(!node.left.left && !node.left.right){
-                sum+= node.left.val;
+    // Initialize the sum variable to store the sum of left leaves
+    let sum = 0;
+    
+    // Loop until the queue is empty
+    while (que.length > 0) {
+        // Dequeue the first node from the queue
+        const node = que.shift();
+        
+        // Check if the left child of the current node exists
+        if (node.left) {
+            // If the left child is a leaf node (both left and right children are null),
+            // add its value to the sum
+            if (!node.left.left && !node.left.right) {
+                sum += node.left.val;
             }
-            else{
-                que.push(node.left)
+            // If the left child is not a leaf node, enqueue it for further processing
+            else {
+                que.push(node.left);
             }
         }
-        if(node.right){
-            que.push(node.right)
+        
+        // If the right child of the current node exists, enqueue it for further processing
+        if (node.right) {
+            que.push(node.right);
         }
     }
-    return sum
+    
+    // Return the sum of left leaves
+    return sum;
 };
