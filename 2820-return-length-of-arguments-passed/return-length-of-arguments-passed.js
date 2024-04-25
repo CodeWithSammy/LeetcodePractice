51654b5309d78be7e2f5f03528a940a1ce4147bf
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-04-25 17:21:57 +0530

    Time: 56 ms (28.50%) | Memory: 48.6 MB (68.14%) - LeetSync

---
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -0,0 +1,19 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+// var argumentsLength = function(...args) {
+//     return args.length;
+// };
+var argumentsLength = function (...args) {
+    let count = 0;
+    for (let i = 0; i < args.length; i++) {
+        count++;
+    }
+    return count;
+
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
