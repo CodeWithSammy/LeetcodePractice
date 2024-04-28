f1134f7205a2b43fd8dfa8cf8a7c3dc9aa869ac2
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-04-29 01:30:20 +0530

    Time: 317 ms (65.22%) | Memory: 85.1 MB (65.22%) - LeetSync

---
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var sumOfDistancesInTree = function(n, edges) {
+    const graph = new Array(n).fill(null).map(() => []);
+    const count = new Array(n).fill(0);
+    const res = new Array(n).fill(0);
+    
+    for (const [u, v] of edges) {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    
+    const dfs1 = (cur, parent) => {
+        count[cur] = 1;
+        for (const child of graph[cur]) {
+            if (child !== parent) {
+                dfs1(child, cur);
+                count[cur] += count[child];
+                res[cur] += res[child] + count[child];
+            }
+        }
+    };
+    
+    const dfs2 = (cur, parent) => {
+        for (const child of graph[cur]) {
+            if (child !== parent) {
+                res[child] = res[cur] + n - 2 * count[child];
+                dfs2(child, cur);
+            }
+        }
+    };
+    
+    dfs1(0, -1);
+    dfs2(0, -1);
+    
+    return res;
+};
