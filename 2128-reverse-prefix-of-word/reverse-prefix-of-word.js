bdf745a8c23c2d6f1faa407ca3d9ea193f1c1d58
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-05-01 23:58:05 +0530

    Time: 47 ms (83.66%) | Memory: 49.1 MB (26.46%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function(word, ch) {
+    return word.indexOf(ch) !== -1 ? word.split("").slice(0, word.indexOf(ch) + 1).reverse().join("") + word.slice(word.indexOf(ch) + 1) : word;
+};
