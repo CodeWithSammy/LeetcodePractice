3f390f8d39c509388e96a36b056464eab91141c6
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2024-05-03 23:54:14 +0530

    Time: 58 ms (23.85%) | Memory: 48.8 MB (53.46%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    var v1Array = version1.split('.');
+    var v2Array = version2.split('.');
+    var length = Math.max(v1Array.length, v2Array.length);
+    
+    for (var i = 0; i < length; i++) {
+        var num1 = parseInt(v1Array[i]) || 0;
+        var num2 = parseInt(v2Array[i]) || 0;
+        
+        if (num1 == num2) {
+            continue;
+        }
+        
+        return num1 > num2 ? 1 : -1;
+    }
+    
+    return 0;       
+};
