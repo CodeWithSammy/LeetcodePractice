9752c691e84f3afb0c669be2ee9f5d9f664fda52
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2025-08-03 01:48:19 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (54.24%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,6 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let Sort = nums.sort()
-    return Sort
+    let low =0, mid =0, high = nums.length-1;
+
+    while(mid <= high){
+        if( nums[mid] === 0){
+            [nums[low], nums[mid]] = [ nums[mid], nums[low]]
+            low++;
+            mid++;
+        } else if(nums[mid] === 1){
+            mid++;
+        } else{
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
+
+    
 };
