99f4ad552f75761a26966fa160a173e5c70cb522
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2025-08-03 01:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (86.13%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -2,21 +2,37 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-    let low =0, mid =0, high = nums.length-1;
+var sortColors = function (nums) {
+    let low = 0, mid = 0, high = nums.length - 1;
 
-    while(mid <= high){
-        if( nums[mid] === 0){
-            [nums[low], nums[mid]] = [ nums[mid], nums[low]]
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]]
             low++;
             mid++;
-        } else if(nums[mid] === 1){
+        } else if (nums[mid] === 1) {
             mid++;
-        } else{
+        } else {
             [nums[mid], nums[high]] = [nums[high], nums[mid]];
             high--;
         }
     }
-
-    
 };
+
+/**
+Core Idea
+Use three pointers:
+
+low: the position to place the next 0
+
+mid: the current element being examined
+
+high: the position to place the next 2
+
+Traverse the array with mid:
+
+If the element is 0: swap with low, then move both low and mid forward
+
+If it's 1: just move mid forward
+
+If it's 2: swap with high, then move high backward (don’t move mid yet) */
