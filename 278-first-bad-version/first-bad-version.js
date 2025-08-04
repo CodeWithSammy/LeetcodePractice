72f2bfcf8592e564c63ba2f4f08258538c144b7e
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2025-08-04 23:52:51 +0530

    Time: 31 ms (95.22%) | Memory: 53.6 MB (38.93%) - LeetSync

--- 278-first-bad-version/first-bad-version.js
+++ 278-first-bad-version/first-bad-version.js
@@ -12,20 +12,23 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
-var solution = function(isBadVersion) {
-    return function(n) {
-        let left = 1, right = n;
-        
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let left = 1, right = n
         while (left < right) {
             let mid = Math.floor((left + right) / 2);
-            
             if (isBadVersion(mid)) {
-                right = mid; // First bad version is at mid or before
-            } else {
-                left = mid + 1; // First bad version is after mid
+
+                right = mid;
+            }
+            else {
+                left = mid + 1
             }
         }
-
-        return left; // Both pointers meet at the first bad version
+        return left
     };
 };
