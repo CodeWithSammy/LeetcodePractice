cda8793262d0d1e1ac50e128bb211310ccf38c10
Samuel Rodrigues <81487415+CodeWithSammy@users.noreply.github.com>
2025-08-08 23:53:02 +0530

    Time: 8 ms (46.95%) | Memory: 64.4 MB (55.39%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -1,12 +1,15 @@
 /**
  * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
+  * @return {boolean}
+   */
+var isPalindrome = function (x) {
     // if x is less than 0 then we return false, else we convert into string, 
     // split it, reverse and then join back again
-    if(x < 0 ){
+    if (x < 0) {
         return false;
     }
-    return  x === +x.toString().split("").reverse().join("");
+    return x === +x.toString().split("").reverse().join("");
 };
+
+
+
